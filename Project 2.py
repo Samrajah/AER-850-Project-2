d99f4aadde1369fbca22fa1646e1ae6986db908d
Samrajah <144837631+Samrajah@users.noreply.github.com>
2023-11-26 17:44:13 -0500

    Update Project 2.py

--- Project 2.py
+++ Project 2.py
@@ -1,4 +1,4 @@
-
+# Step 1: Data Processing
 from keras.preprocessing.image import ImageDataGenerator
 
 # Define input image shape
@@ -36,3 +36,57 @@ validation_generator = validation_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical'
 )
+
+# Step 2: Neural Network Analysis
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+# Define input image shape
+input_shape = (100, 100, 3)
+
+# Build the neural network
+model = models.Sequential()
+
+# Convolutional layers
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+# Flatten layer
+model.add(layers.Flatten())
+
+# Fully connected layers
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.5))  # Dropout layer to reduce overfitting
+
+# Output layer
+model.add(layers.Dense(4, activation='softmax'))  # Assuming 4 output classes
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',  # Adjust loss function based on your task
+              metrics=['accuracy'])
+
+# Display the model summary
+model.summary()
+
+# Step 3: Neural Network Analysis
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=input_shape[:2],
+    batch_size=batch_size,
+    class_mode='categorical'
+)
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(test_generator)
+
+# Print the evaluation results
+print(f'Test Loss: {test_loss}')
+print(f'Test Accuracy: {test_accuracy}')
