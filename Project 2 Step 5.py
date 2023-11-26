cde045a9e3ebb7b4c0c08727f98ff7f7da05d05e
Samrajah <144837631+Samrajah@users.noreply.github.com>
2023-11-26 18:37:35 -0500

    Update Project 2 Step 5.py

--- Project 2 Step 5.py
+++ Project 2 Step 5.py
@@ -1,13 +1,15 @@
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
 
 def load_and_preprocess_image(image_path):
     # Load and preprocess the test image
     test_image = image.load_img(image_path, target_size=(100, 100))
     test_image_array = image.img_to_array(test_image)
     test_image_array = np.expand_dims(test_image_array, axis=0)
-    test_image_array /= 255.  # Normalize the image
+    test_image_array /= 255.0  # Normalize the image
     return test_image_array
 
 def make_prediction(model, test_image_array):
@@ -20,7 +22,7 @@ def make_prediction(model, test_image_array):
 
 def main():
     # Define the path to the test image
-    test_image_path = r'C:\Users\samby\OneDrive\Documents\GitHub\AER-850-Project-2\Project 2 Data\Data\Test\Medium'  # Replace with the actual path
+    test_image_path = r'C:\Users\samby\OneDrive\Documents\GitHub\AER-850-Project-2\Project 2 Data\Data\Test\Medium'  
 
     # Load the trained model
     model = load_model('Project_2_model.h5')
